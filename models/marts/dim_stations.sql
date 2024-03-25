7a703bf19388c23aaeba1f446c9376350ee884ee
Andrew Lui <andrew.lui@pepsico.com>
2024-03-24 20:07:02 -0400

    Alui 6/documentation and testing (#8)
    * change the readme a bit
    
    * long-term goals
    
    * gitignore changes
    
    * move profiles
    
    * new macros
    
    * move files around
    
    * rename integration macro
    
    * additional yamls
    
    * adds documentation and some basic testing
    
    * adds the viz to the readme as well
    
    ---------
    
    Co-authored-by: alui <andrewhlui@gmail.com>

--- models/marts/dim_stations.sql
+++ models/marts/dim_stations.sql
@@ -5,14 +5,14 @@
     )
 }}
 
+with cte_stations as (
 {% for type in ['start', 'end'] %}
 
 select distinct
     {{ type }}_station_name as station_name,
-    {{ type }}_station_id as station_id
+    {{ type }}_station_id as station_id,
     {{ type }}_lat as latitude,
-    {{ type }}_lng as longitude, 
-    '{{ type }}' as source, 
+    {{ type }}_lng as longitude,
     load_dts
 from
     {{ ref('stg_citibike') }}
@@ -25,3 +25,16 @@ where
 {% if not loop.last -%} union {%- endif %}
 
 {% endfor %}
+
+)
+
+select
+    station_name,
+    station_id,
+    latitude,
+    longitude
+from
+    cte_stations
+--lat/long is not unique
+qualify
+    row_number() over (partition by station_id order by latitude, longitude) = 1
